7c6e6fab6a4fe5f78d87a2f91ad2d0959c284599
jgunter7 <jason_gunter@hotmail.com>
2016-10-02 16:58:26 -0400

    Fixed the repo destruction

---
+++ ZombieTakeover/Assets/_Scripts/ItemsController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*      File Name:              ItemsController.cs
+ *      Author's Name:          Jason Gunter
+ *      Last Modified By:       Jason Gunter
+ *      Date Last Modified:     Oct 2nd, 2016
+ *      Program Description:    A 2D scrolling game
+ *      File Description:       This script controls the items that are 
+ *                              dropped by the enemy ship
+ *      Revision History:       https://github.com/jgunter7/COMP305_ZombieTakeOver_As1
+ */
+public class ItemsController : MonoBehaviour {
+    // --------------------- PRIVATE VARIABLES
+    private int _speed;
+    private Transform _transform;
+
+    void Start() {
+        //init private variables
+        this._transform = this.GetComponent<Transform>();
+        this._speed = 2;
+    }
+
+    void Update() {
+        this._move();
+        this._checkBounds();
+    }
+
+    private void _move() {
+        //update the item position based on its speed.
+        Vector2 newPosition = this._transform.position;
+        newPosition.y -= this._speed;
+        this._transform.position = newPosition;
+    }
+
+    private void _checkBounds() {
+        // destroy items that scroll past the screens viewport
+        if (this._transform.position.y <= -270f) {
+            this._destroy();
+        }
+    }
+    private void _destroy() {
+        Destroy(this.gameObject); //remove the item from the screen once it has passed the player ship
+    }
+}
---
+++ ZombieTakeover/Assets/_Scripts/ShipController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*      File Name:              ShipController.cs
+ *      Author's Name:          Jason Gunter
+ *      Last Modified By:       Jason Gunter
+ *      Date Last Modified:     Oct 2nd, 2016
+ *      Program Description:    A 2D scrolling game
+ *      File Description:       This script controls the player ship's movements, 
+ *                              actions, collsions, score, lives, and sound
+ *      Revision History:       https://github.com/jgunter7/COMP305_ZombieTakeOver_As1
+ */
+public class ShipController : MonoBehaviour {
+    //PRIVATE VARIABLES
+    private Transform _transform;
+    private Vector2 _currentPos;
+    private float _playerInput;
+    private float _speed;
+
+    // PUBLIC VARIABLES
+    public Transform explosion;
+    public GameController gameController;
+    public AudioSource evilLaughSound;
+    public AudioSource collectSound;
+    public AudioSource explosionSound;
+
+	void Start () {
+        this._speed = 2;
+        this._transform = this.GetComponent<Transform>();
+	}
+
+	void Update () {
+        this._move();     
+	}
+
+    private void _move() {
+        // Move the main ship with the keyboard controls.
+        this._currentPos = this._transform.position;
+        this._playerInput = Input.GetAxis("Horizontal");
+
+        if (this._playerInput > 0) {
+            this._currentPos += new Vector2(this._speed, 0);
+        } else if (this._playerInput < 0) {
+            this._currentPos -= new Vector2(this._speed, 0);
+        }
+
+        this._transform.position = new Vector2(Mathf.Clamp(this._currentPos.x, -290f, 290f), -165f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D theCol) {
+        // ** //\\ ** \\--- Collision Detection - Gas Tanks / Boats / Enemies (bombs) ---// ** //\\ ** \\
+        if (theCol.gameObject.CompareTag("Gas")) {
+            this.gameController.ScoreValue += 50;
+            this.collectSound.Play();
+        } else if (theCol.gameObject.CompareTag("Enemy")) {
+            this.gameController.LivesValue -= 1;
+            this.explosionSound.Play();
+            GameObject exploder = ((Transform)Instantiate(explosion, theCol.gameObject.transform.position, Quaternion.identity)).gameObject;
+            Destroy(exploder, 2.0f);        
+        } else if (theCol.gameObject.CompareTag("Boat")) {
+            this.gameController.ScoreValue += 50;
+            this.explosionSound.Play();
+            this.evilLaughSound.Play();
+            GameObject exploder = ((Transform)Instantiate(explosion, theCol.gameObject.transform.position, Quaternion.identity)).gameObject;
+            Destroy(exploder, 1.5f);
+        }
+        //regardless, the colliding object needs to be removed from the scene
+        theCol.gameObject.transform.position = new Vector3(theCol.gameObject.transform.position.x, -300);
+    }
+}
